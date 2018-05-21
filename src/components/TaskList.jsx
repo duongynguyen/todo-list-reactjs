58fc02957c7a6d8fff5c4d62d69a838044eb42a8
Nguyen Duong <duongynguyen0501@gmail.com>
2018-05-21 08:16:50 +0700

    add first

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import TaskItem from './TaskItem.jsx';
+
+class TaskList extends Component {
+
+
+  render() {
+    var { tasks } = this.props; // var tasks = this.props.tasks;
+    var elementTasks = tasks.map((task, index) => {
+        return <TaskItem 
+                    key={task.id} 
+                    index={index + 1} 
+                    task={task} 
+                    onUpdateStatus={this.props.onUpdateStatus} 
+                    onDelete={this.props.onDelete}
+                    onUpdate={this.props.onUpdate}
+                />
+    });
+    return (      
+        <table className="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th className="text-center">STT</th>
+                    <th className="text-center">Tên</th>
+                    <th className="text-center">Trạng Thái</th>
+                    <th className="text-center">Hành Động</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type="text" className="form-control" />
+                    </td>
+                    <td>
+                        <select className="form-control">
+                            <option value="-1">Tất Cả</option>
+                            <option value="0">Ẩn</option>
+                            <option value="1">Kích Hoạt</option>
+                        </select>
+                    </td>
+                    <td></td>
+                </tr>
+                { elementTasks }
+            </tbody>
+        </table>
+    );
+  }
+}
+
+export default TaskList;
